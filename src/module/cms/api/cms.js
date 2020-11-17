4a5d6b0d41a7f92e0616470b3c2307a51a0de5a8
xuqiangsheng <xuqiangsheng@rfchina.com>
2020-11-17 16:35:14 +0800

    feat(页面管理):day03 finish

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -17,4 +17,21 @@ export const page_add = params =>{
   return http.requestPost(apiUrl+'/cms/page/add/',params);
 }
 
+//页面编辑
+export const page_edit = (id,params) =>{
+  //请求服务端的页面查询接口
+  return http.requestPut(apiUrl+'/cms/page/edit/' + id,params);
+}
+
+//页面查询
+export const page_get = id =>{
+  //请求服务端的页面查询接口
+  return http.requestQuickGet(apiUrl+'/cms/page/get/' + id);
+}
+
+//页面删除
+export const page_del = id =>{
+  return http.requestDelete(apiUrl + '/cms/page/del/' + id);
+}
+
 
--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
     <!--编写页面静态部分，即view部分-->
-<!--    查询表单-->
+    <!--查询表单-->
     <el-form :model="params">
-      <el-select v-model="params.siteId" placeholder="请选择站点">
-        <el-option v-for = "item in siteList"
-                   :key = "item.siteId"
-                   :label="item.siteName"
-                   :value="item.siteId">
+      站点名称：<el-select v-model="params.siteId" placeholder="请选择站点">
+        <el-option
+          v-for="item in siteList"
+          :key="item.siteId"
+          :label="item.siteName"
+          :value="item.siteId">
         </el-option>
       </el-select>
-      页面别名:
-      <el-input v-model="params.pageAliase" style="width: 100px"></el-input>
+      页面名称：<el-input v-model="params.pageName"  style="width: 100px"></el-input>
+      页面别名：<el-input v-model="params.pageAliase"  style="width: 100px"></el-input>
+      页面类型：<el-select v-model="params.pageType" placeholder="请选择页面类型">
+        <el-option
+          v-for="item in pageTypeList"
+          :key="item.pageTypeId"
+          :label="item.pageTypeName"
+          :value="item.pageTypeId">
+        </el-option>
+      </el-select>
       <el-button type="primary" size="small" v-on:click="query">查询</el-button>
-      <router-link class="mui-tab-item" :to="{path:'/cms/page/add/',query:{
-        page: this.params.page,
-        siteId: this.params.siteId
+      <router-link :to="{path:'/cms/page/add',query:{
+        page:this.params.page,
+        siteId:this.params.siteId
       }}">
-        <el-button type="primary" size="small">新增页面</el-button>
+         <el-button  type="primary" size="small">新增页面</el-button>
       </router-link>
     </el-form>
+
     <el-table
       :data="list"
       stripe
@@ -38,6 +48,19 @@
       </el-table-column>
       <el-table-column prop="pageCreateTime" label="创建时间" width="180" >
       </el-table-column>
+      <el-table-column label="操作" width="80">
+        <template slot-scope="page">
+          <el-button
+            size="small"type="text"
+            @click="edit(page.row.pageId)">编辑
+          </el-button>
+          <el-button
+            size="small"type="text"
+            @click="del(page.row.pageId)">删除
+          </el-button>
+        </template>
+
+      </el-table-column>
     </el-table>
     <el-pagination
       layout="prev, pager, next"
@@ -55,19 +78,22 @@
   export default {
     data() {
       return {
-        siteList: [], //站点列表
+        siteList:[],//站点列表
+        pageTypeList:[],//页面类型列表
         list: [],
-        total:50,
+        total:0,
         params:{
-          page:1, //页码
-          size:10, //每页显示个数
-          siteId:'', //站点id
-          pageAliase:'' //站点别名
+          siteId:'',
+          pageAliase:'',
+          pageName:'',
+          pageType:'',
+          page:1,
+          size:10
         }
       }
     },
     methods:{
-
+      //页面查询
       query:function(){
         // alert('查询')
         //调用服务端的接口
@@ -78,32 +104,63 @@
         })
 
       },
-      //分页查询，接收page页码
       changePage:function(page){//形参就是当前页码
         //调用query方法
         // alert(page)
         this.params.page = page;
         this.query()
+      },
+      edit:function(pageId){
+        //打开修改页面
+        this.$router.push({
+          path:'/cms/page/edit/'+pageId
+        })
+      },
+      del:function (pageId) {
+        this.$confirm('您确认删除吗?', '提示', { }).then(() => {
+          //调用服务端接口
+          cmsApi.page_del(pageId).then(res=>{
+
+            if(res.success){
+              this.$message.success("删除成功")
+              //刷新页面
+              this.query()
+            }else{
+              this.$message.error("删除失败")
+            }
+          })
+        })
+
       }
     },
-    created() {
-      //从路由上获取参数信息
-      this.params.page = Number.parseInt(this.$route.query.page || 1);
-      this.params.siteId = this.$route.query.siteId || '';
+    created(){
+      //取出路由中的参数，赋值给数据对象
+      this.params.page = Number.parseInt(this.$route.query.page || 1)
+      this.params.siteId = this.$route.query.siteId || ''
+
     },
-    //进入页面即查询
     mounted(){
       //当DOM元素渲染完成后调用query
       this.query()
       //初始化站点列表
-      this.siteList =[
+      this.siteList = [
         {
           siteId:'5a751fab6abb5044e0d19ea1',
-          siteName:'首页'
+          siteName:'门户主站'
         },
         {
-          siteId:'5a751fab6abb5044e0d19ea1',
-          siteName:'轮播图'
+          siteId:'102',
+          siteName:'测试站'
+        }
+      ]
+      this.pageTypeList = [
+        {
+          pageTypeId:'0',
+          pageTypeName:'静态'
+        },
+        {
+          pageTypeId:'1',
+          pageTypeName:'动态'
         }
       ]
     }
